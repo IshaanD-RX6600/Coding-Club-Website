9139ef991cdd629e59b110018a295aa452f2d9e4
DhairyaS450 <dhairyashah2513@gmail.com>
2025-03-12 18:04:05 -0400

    add one space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Navigation } from "@/components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CHCI CodingClub",
+  title: "CHCI Coding Club",
   description: "Welcome to the CHCI Coding Club website",
 };
 
